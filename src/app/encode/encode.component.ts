f611241e559d8bc6ab2544dfaf814dfb66eca1cd
tom <tombuehler@ymail.com>
2023-05-09 21:52:05 +0200

    use escape to cancel encoding

--- src/app/encode/encode.component.ts
+++ src/app/encode/encode.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
-import { Observable, concatMap, endWith, filter, from, fromEvent, map, scan, tap } from 'rxjs';
+import { Observable, concatMap, endWith, filter, from, fromEvent, map, scan, startWith, switchMap, tap } from 'rxjs';
 import { debug } from '../debug-operator';
 import { delayEach } from '../delay-each';
 import { CHAR_END, MAP } from '../morse';
@@ -20,33 +20,57 @@ export class EncodeComponent {
   public typedText$: Observable<string>;
 
   constructor(@Inject(MORSE_ALPHABET) alphabet: Map<Letter, MorseSignal[]>,
-              @Inject(DOT_DURATION_IN_MS) private dotTimeInMs: number)   {
+    @Inject(DOT_DURATION_IN_MS) private dotTimeInMs: number) {
+
     const keyDownEvents$ = fromEvent<KeyboardEvent>(document, 'keydown').pipe(
       filter(event => event.key.length == 1)
     );
 
-    this.typedText$ = keyDownEvents$.pipe(
-      scan((acc, { key }) => acc + key, '')
+    const escapePressed$ = fromEvent<KeyboardEvent>(document, 'keyup').pipe(
+      filter(({ key }) => key === 'Escape'),
+      map(_ => undefined),
     );
+
+    this.typedText$ = escapePressed$.pipe(
+      startWith(undefined),
+      switchMap(_ => keyDownEvents$.pipe(
+        scan((acc, { key }) => acc + key, ''),
+        startWith('')
+      )),
+    );
+
+    //.subscribe(event => console.log('event', event))
+
+    // this.typedText$ = keyDownEvents$.pipe(
+    //   scan((acc, { key }) => acc + key, '')
+    // );
+
+
 
     /*
     - dash: three 1s in a row
     - dot: sigle 1
     - separated by a single 0
     - letters are separated by three 0s
-    - words are separated by seven 0s (four 0's = space + three 0s of char end)
+    - words are separated by sev;en 0s (four 0's = space + three 0s of char end)
     */
-    this.signal$ = keyDownEvents$.pipe(
-      map(({ key }) => alphabet.get(key as Letter)),
-      filter(Boolean),
-      map(morseSignals => morseSignals
-        .map(each => (each === '.' ? [1] : [1, 1, 1]) as (0 | 1)[])
-        .reduce((prev, curr) => (prev.length > 0) ? [...prev, 0, ...curr] : curr, [])
-      ),
-      concatMap(sequence => from(sequence).pipe(
-        endWith(...CHAR_END),
-      )),
-      delayEach(2 * dotTimeInMs),
+    this.signal$ = escapePressed$.pipe(
+      startWith(undefined),
+      switchMap(_ =>
+        keyDownEvents$.pipe(
+          map(({ key }) => alphabet.get(key as Letter)),
+          filter(Boolean),
+          map(morseSignals => morseSignals
+            .map(each => (each === '.' ? [1] : [1, 1, 1]) as (0 | 1)[])
+            .reduce((prev, curr) => (prev.length > 0) ? [...prev, 0, ...curr] : curr, [])
+          ),
+          concatMap(sequence => from(sequence).pipe(
+            endWith(...CHAR_END),
+          )),
+          delayEach(2 * dotTimeInMs),
+          startWith(0 as 0),
+        )
+      )
     );
   }
 }
